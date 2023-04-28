6b3c8eb384e3587d06dce2edb421ec625ab00444
ofemjohn <ofemjohn@gmail.com>
2023-04-28 03:42:31 +0100

    bycripting

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python3
 '''Using Bcript for password encryptions'''
 import bcrypt
-import logging
-
-logging.basicConfig(level=logging.INFO)
 
 
 def hash_password(password: str) -> bytes:
     '''hashes a password'''
-    try:
-        encoded_password = password.encode()
-        hashed_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
-        logging.info("Password successfully hashed")
-        return hashed_password
-    except Exception as e:
-        logging.error("Error hashing password: %s", e)
+    encoded_password = password.encode()
+    hashed_password = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
+    return hashed_password
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     '''checks if password and the hashed password corresponds'''
-    valid = False
-    try:
-        encoded_password = password.encode()
-        if bcrypt.checkpw(encoded_password, hashed_password):
-            valid = True
-            logging.info("Password is valid")
-        else:
-            logging.warning("Password is invalid")
-    except Exception as e:
-        logging.error("Error validating password: %s", e)
-    return valid
+    iss_valid = False
+    encoded_password = password.encode()
+    if bcrypt.checkpw(encoded_password, hashed_password):
+        iss_valid = True
+    return iss_valid
