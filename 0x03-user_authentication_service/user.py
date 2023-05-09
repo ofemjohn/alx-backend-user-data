7efc94a1df37aee024aca54034a4ee64b2311d3a
ofemjohn <ofemjohn@gmail.com>
2023-05-09 22:36:00 +0100

    corrected pycodestyle for user orm model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-'''user model for a database connection to a database server'''
-from sqlalchemy import Column, Integer, String
-#from sqlalchemy.ext.declarative import declarative_base
+'''
+user model for a database connection to a database server
+from sqlalchemy.ext.declarative import declarative_base
+'''
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
 
 
 Base = declarative_base()
@@ -15,5 +17,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     email = Column(String(255), nullable=False)
     hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250),nullable=True)
+    session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=False)
