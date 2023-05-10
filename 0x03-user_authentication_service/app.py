0a372ee3fdf6791f406869ae97ac7361de41f3c7
ofemjohn <ofemjohn@gmail.com>
2023-05-10 23:44:33 +0100

    simple flask app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 '''A basic flask application'''
-from flask import Flask
-from flask import jsonify
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def welcome() -> str:
     '''A simple route'''
     return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
