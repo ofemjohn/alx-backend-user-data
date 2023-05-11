a881b19fc4cba8520029317d47c5a89b7c36acc3
ofemjohn <ofemjohn@gmail.com>
2023-05-11 23:23:40 +0100

    reset password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,5 +96,23 @@ def reset_password_token() -> str:
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    '''route to reset password'''
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+    except KeyError:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token=reset_token, password=new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5001')
