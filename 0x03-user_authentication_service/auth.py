1b55087727c7f255d6c5d228c1f5df80b4cdd284
ofemjohn <ofemjohn@gmail.com>
2023-05-11 14:06:20 +0100

    updated the auth module with a create_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,17 @@ class Auth:
             return True
         else:
             return False
+
+    def create_session(self, email: str) -> str:
+        '''
+        Create a new session for the user corresponding to the provided email.
+        Returns the session ID as a string.
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
