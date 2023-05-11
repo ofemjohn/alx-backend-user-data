788d27285643f4e1d25f85a4251f270cfc49fefe
ofemjohn <ofemjohn@gmail.com>
2023-05-11 15:06:56 +0100

    added an updated method to destrou session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,3 +84,13 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        '''find user by id and destroy session if it exists'''
+        try:
+            user = self._db.find_user_by(id=user_id)
+        except NoResultFound:
+            return None
+
+        self._db.update_user(user, session_id=None)
+        return None
