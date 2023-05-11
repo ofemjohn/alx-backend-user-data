827e8e900fa1d4461ee942aab28fe5d64059ab4c
ofemjohn <ofemjohn@gmail.com>
2023-05-11 11:38:28 +0100

    created a user route for creating users with email and password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 '''A basic flask application'''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -11,5 +13,21 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'])
+def register_users() -> str:
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        return jsonify({'message': 'missing email or password'}), 400
+
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({'message': 'email already registered'}), 400
+
+    return jsonify({'email': email, 'message': 'user created'})
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5001')
